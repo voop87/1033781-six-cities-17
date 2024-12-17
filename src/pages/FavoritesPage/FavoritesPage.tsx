6b598307b4a4c92e9d7795ae55e11816912b124f
Ruslan Mansurov <mansurov87@gmail.com>
2024-12-17 16:47:04 +0700

    добавляет отрисовку пустой страницы Favorites

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -30,12 +30,27 @@ function FavoritesPage({ favoritesList }: FavoritesProps) {
         </main>
       )}
 
-      {favoritesList && (
+      {favoritesList ? (
         <main className="page__main page__main--favorites">
           <div className="page__favorites-container container">
             <section className="favorites">
               <h1 className="favorites__title">Saved listing</h1>
               <FavoritesList favoritesList={favoritesList} />
+            </section>
+          </div>
+        </main>
+      ) : (
+        <main className="page__main page__main--favorites page__main--favorites-empty">
+          <div className="page__favorites-container container">
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">Favorites (empty)</h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">Nothing yet saved.</b>
+                <p className="favorites__status-description">
+                  Save properties to narrow down search or plan your future
+                  trips.
+                </p>
+              </div>
             </section>
           </div>
         </main>
